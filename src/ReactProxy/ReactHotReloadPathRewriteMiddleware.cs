ef5f56960df1b28636d65867caa53def6ca435d3
Damian Hickey <dhickey@gmail.com>
2020-04-05 14:29:20 +0200

    Sample react app proxying on different path and with websockets

---
+++ src/ReactProxy/ReactHotReloadPathRewriteMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Http;
+
+namespace ReactProxy
+{
+    [UsedImplicitly]
+    public class ReactHotReloadPathRewriteMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string _spaPath;
+
+        public ReactHotReloadPathRewriteMiddleware(RequestDelegate next, string spaPath)
+        {
+            _next = next;
+            _spaPath = spaPath;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            // Workaround the websocket URL for reloading the SPA app is a rooted url.
+            // Here we are rewriting the path so the request is handled by the SPA middleware.
+            var path = context.Request.Path;
+            if (path.HasValue &&
+                (path.StartsWithSegments("/sockjs-node")
+                 || path.StartsWithSegments("/static")
+                 || path.Value.Equals("/manifest.json")
+                 || path.Value.EndsWith("hot-update.json")
+                 || path.Value.EndsWith("hot-update.js")))
+            {
+                context.Request.Path = new PathString(_spaPath + context.Request.Path);
+            }
+            await _next(context);
+        }
+    }
+}
---
+++ src/ReactProxy/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using ProxyKit;
+
+namespace ReactProxy
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddProxy();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseWebSockets();
+            app.UseMiddleware<ReactHotReloadPathRewriteMiddleware>("/spa");
+            app.Map("/spa", spaApp =>
+            {
+                spaApp.UseWebSocketProxy(context => "ws://localhost:3000");
+                spaApp.RunProxy(context => 
+                    context
+                        .ForwardTo("http://localhost:3000")
+                        .Send());
+            });
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
